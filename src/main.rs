933101a769c3bae332ed0aa6a373fdf04a2dca04
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-03-23 17:46:21 -0500

    Set up example

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate llg;
+extern crate nalgebra;
+
+use llg::{Integrator};
+use nalgebra::{Vector3};
+
+fn main() {
+    let integrator = Integrator::new(0.1);
+    let mut spin = Vector3::new(1.0, 0.0, 0.0);
+    let field = Vector3::new(0.0, 0.0, 0.05);
+    for _ in 0..1_000_000 {
+        println!("{} {} {}", spin.x, spin.y, spin.z);
+        spin = integrator.step(&spin, &field, 1.0e-2);
+    }
+}
