a0329c7e7469674d3d66b0869e9777e36067e62b
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-03-24 13:06:03 -0500

    What I got in the seminar

--- src/lib.rs
+++ src/lib.rs
@@ -2,124 +2,83 @@ extern crate nalgebra;
 
 use nalgebra::{Vector3, cross, norm};
 
-
 #[derive(Debug)]
-pub struct Integrator {
-    // Gilbert damping parameter, should be 1 for critical
-    damping: f64,
-    // Absolute value of the gyromagnetic ratio
-    giromagnetic_ratio: f64,
-    // This prefactor is always used so might as well cahe it
+pub struct HeunIntegrator {
+    gamma: f64,
+    lambda: f64,
     prefactor: f64,
 }
 
 
-impl Integrator {
-     pub fn new(damping: f64) -> Integrator {
-         let giromagnetic_ratio = 1.0; // 1.760859644e-11; // rad s^-1 T^-1
-         Integrator {
-             damping: damping,
-             giromagnetic_ratio: giromagnetic_ratio,
-             prefactor: giromagnetic_ratio / (1.0f64 + damping * damping),
-         }
-     }
+impl HeunIntegrator {
+    pub fn new(lambda: f64) -> HeunIntegrator {
+        HeunIntegrator {
+            gamma: 1.0,
+            lambda: lambda,
+            prefactor: 1.0 / (1.0 + lambda * lambda)
+        }
+    }
 
-     pub fn critical() -> Integrator {
-         Integrator::new(1.0f64)
-     }
-
-     /// Little wrapper for that ugly delta function
-     fn delta(&self, spin: &Vector3<f64>, field: &Vector3<f64>) -> Vector3<f64> {
+    fn delta(&self, spin: &Vector3<f64>, field: &Vector3<f64>) -> Vector3<f64> {
         - self.prefactor * (
             cross(spin, field) +
-            self.damping * cross(spin, &cross(spin, field))
+            self.lambda * &cross(spin, &cross(spin, field))
         )
-     }
+    }
 
-     /// Performs a heun step over a spin, assuming the field
-     /// independent of the spin change.
-     pub fn step(&self, spin: &Vector3<f64>, field: &Vector3<f64>, time: f64) -> Vector3<f64> {
-        // This is the predictor step
-        let delta_spin = self.delta(spin, field);
-        let spin_prima = spin + time * delta_spin;
+    pub fn step(&self, spin: &Vector3<f64>, field: &Vector3<f64>, time: f64) -> Vector3<f64> {
+        let delta = self.delta(spin, field);
+        let spin_prima = spin + delta * time;
         let spin_prima = spin_prima / norm(&spin_prima);
-        let delta_spin_prima = self.delta(&spin_prima, field);
-        let final_spin = spin + 0.5 * time * (delta_spin + delta_spin_prima);
-        final_spin / norm(&final_spin)
-     }
+        // I should have computed field_prima
+        let delta_prima = self.delta(&spin_prima, field);
+        let new_spin = spin + 0.5 * (delta + delta_prima) * time;
+        new_spin / norm(&new_spin)
+    }
 }
 
 
 #[cfg(test)]
-mod tests {
-    use nalgebra::{Vector3, cross, norm, approx_eq};
-    use super::{Integrator};
+mod test {
+    use nalgebra::{Vector3, cross, norm};
+    use super::{HeunIntegrator};
 
     #[test]
     fn cross_product_works() {
-        let i = Vector3::new(1f64, 0f64, 0f64);
-        let j = Vector3::new(0f64, 1f64, 0f64);
-        let k = Vector3::new(0f64, 0f64, 1f64);
+        let i = Vector3::new(1.0, 0.0, 0.0);
+        let j = Vector3::new(0.0, 1.0, 0.0);
+        let k = Vector3::new(0.0, 0.0, 1.0);
         assert_eq!(cross(&i, &j), k);
         assert_eq!(cross(&j, &k), i);
-        assert_eq!(cross(&k, &i), j);
     }
 
     #[test]
-    fn norm_works() {
-        let i = Vector3::new(1f64, 0f64, 0f64);
-        let j = Vector3::new(0f64, 1f64, 0f64);
-        let k = Vector3::new(0f64, 0f64, 1f64);
-        assert_eq!(norm(&i), 1.0);
-        assert_eq!(norm(&j), 1.0);
-        assert_eq!(norm(&k), 1.0);
-        assert_eq!(norm(&(i + j)), (2.0f64).sqrt());
-        assert_eq!(norm(&(i + j + k)), (3.0f64).sqrt());
+    fn norm_works_as_expected() {
+        assert_eq!(norm(&Vector3::new(0.0, 0.0, 1.0)), 1.0);
+        assert_eq!(norm(&Vector3::new(0.0, 1.0, 1.0)), (2.0f64).sqrt());
     }
 
     #[test]
-    fn normalization_works() {
-        let i = Vector3::new(1f64, 0f64, 0f64);
-        let not_normalized = Vector3::new(100f64, 0f64, 0f64);
-        assert_eq!(not_normalized / norm(&not_normalized), i)
+    fn can_create_heun_integrators() {
+        let _integrator = HeunIntegrator::new(1.0);
     }
 
     #[test]
-    fn can_create_integrators() {
-        let _integrator = Integrator::new(1.0);
-        let _integrator = Integrator::critical();
+    fn heun_integrator_is_norm_conserving() {
+        let integrator = HeunIntegrator::new(1.0);
+        let initial_spin = Vector3::new(1.0, 0.0, 0.0);
+        let field = Vector3::new(0.0, 0.0, 1.0);
+        let new_spin = integrator.step(&initial_spin, &field, 1.0e-5);
+        assert_eq!(norm(&new_spin), 1.0);
     }
 
     #[test]
-    fn can_perform_spin_step() {
-        let integrator = Integrator::critical();
-        let old_spin = Vector3::new(1f64, 0f64, 0f64);
-        let field = Vector3::new(0f64, 0f64, 1f64);
-        let delta_time = 1.0e-16;
-        let _new_spin = integrator.step(&old_spin, &field, delta_time);
-    }
-
-    #[test]
-    fn step_moves_spin_towards_field() {
-        let integrator = Integrator::critical();
-        let old_spin = Vector3::new(1f64, 0f64, 0f64);
-        let field = Vector3::new(0f64, 0f64, 1f64);
-        let delta_time = 1.0e-16;
-        let new_spin = integrator.step(&old_spin, &field, delta_time);
+    fn heun_integrator_advances_spin() {
+        let integrator = HeunIntegrator::new(1.0);
+        let initial_spin = Vector3::new(1.0, 0.0, 0.0);
+        let field = Vector3::new(0.0, 0.0, 1.0);
+        let new_spin = integrator.step(&initial_spin, &field, 1.0e-5);
+        assert!(new_spin.x > 0.0);
         assert!(new_spin.y > 0.0);
-        assert!(new_spin.z > 0.0);
-    }
-
-    #[test]
-    fn many_steps_make_the_spin_go_towards_field() {
-        let integrator = Integrator::critical();
-        let field = Vector3::new(0f64, 0f64, 1f64);
-        let delta_time = 1.0e-4;
-        let mut spin = Vector3::new(1f64, 0f64, 0f64);
-        for _ in 0..1000_000 {
-            spin = integrator.step(&spin, &field, delta_time);
-        }
-        println!("{:?}", spin);
-        assert!(approx_eq(&spin, &field));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate llg;
 extern crate nalgebra;
 
-use llg::{Integrator};
+use llg::{HeunIntegrator};
 use nalgebra::{Vector3};
 
 fn main() {
-    let integrator = Integrator::new(0.1);
+    let integrator = HeunIntegrator::new(0.1);
     let mut spin = Vector3::new(1.0, 0.0, 0.0);
     let field = Vector3::new(0.0, 0.0, 0.05);
     for _ in 0..1_000_000 {
